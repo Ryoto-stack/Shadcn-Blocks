110a9374a8791655bfcec3518e9757f970f7cb8c
OJT <ojt@UASREGSOJT2.UASPH.COM>
2024-05-08 15:00:32 +0800

    ordersgap

--- components/Orders.tsx
+++ components/Orders.tsx
@@ -246,7 +246,7 @@ export function Orders() {
           </DropdownMenu>
         </header>
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-          <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+          <div className="grid auto-rows-max items-start gap-4 md:gap-1 lg:col-span-3">
             <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
               <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
                 <CardHeader className="pb-3">
@@ -292,7 +292,7 @@ export function Orders() {
                       Recent Activity from your Inventory
                     </CardDescription>
                     <div className="flex items-center">
-                <div className="ml-auto flex items-center gap-2">
+                <div className="flex items-center gap-2">
                   <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                       <Button
