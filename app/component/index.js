6be60cdde226c6ac9cc064d1d43939dc6a52eb84
OJT <ojt@UASREGSOJT1.UASPH.COM>
2024-05-10 14:01:24 +0800

    buttons

--- app/component/index.js
+++ app/component/index.js
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import styles from './Barchart.module.css'
 
-import { Pie } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -12,9 +12,7 @@ import {
     Title,
     Tooltip,
     Legend,
-    plugins,
-    ArcElement,
-    
+    plugins
 } from 'chart.js'
 
 ChartJS.register(
