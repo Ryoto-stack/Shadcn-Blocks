fe3db5eb0b7b3df85c011610c8be2ec910b079e8
ryoto-stack <blackryoto@gmail.com>
2024-05-12 20:44:43 +0800

    data table

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -3,7 +3,7 @@ import { DataTable } from "@/app/payment/data-table";
 
 async function getUsers(): Promise<User[]> {
   const res = await fetch(
-    "https://66401958a7500fcf1a9d1220.mockapi.io/api/users"
+    "https://66401958a7500fcf1a9d1220.mockapi.io/api/users/Users"
   );
   const data = await res.json();
   return data;
