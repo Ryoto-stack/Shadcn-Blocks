b336e72f5166b8181e4428342de9182b9d61b72e
OJT <ojt@UASREGSOJT2.UASPH.COM>
2024-05-14 15:08:55 +0800

    loginadd

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -1,61 +1,57 @@
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export function SignupForm() {
   return (
-    <Card className="mx-auto max-w-sm h-full">
-      <CardHeader>
-        <CardTitle className="text-xl">Sign Up</CardTitle>
-        <CardDescription>
-          Enter your information to create an account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
+<div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-5">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
+            </p>
+          </div>
+          <div className="grid gap-4">
             <div className="grid gap-2">
-              <Label htmlFor="first-name">First name</Label>
-              <Input id="first-name" placeholder="First Name" required />
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="@uas.com.ph"
+                required
+              />
             </div>
             <div className="grid gap-2">
-              <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" placeholder="Last Name" required />
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input 
+                id="password" 
+                type="password" 
+                placeholder="***********"
+                required 
+              />
             </div>
+            <a href="/dashboard"><Button type="submit" className="w-full py-2">
+              Login
+            </Button></a>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@uas.com.ph"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-          <a href="/dashboard"><Button type="submit" className="w-full py-2">
-            Create an account
-          </Button></a>
         </div>
-        <div className="mt-4 text-center text-sm">
-          Already have an account?{" "}
-          <a href="/login" className="underline">
-            Login
-          </a>
-        </div>
-      </CardContent>
-    </Card>
-  );
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/UASLOGO.jpg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
 }
