1f0503a6a92b92a00e49accd451beca18bcb66f2
OJT <ojt@UASREGSOJT2.UASPH.COM>
2024-05-15 13:56:11 +0800

    create Accessories

---
+++ app/createaccessories/page.tsx
@@ -0,0 +1,10 @@
+import { CreateAccessories } from '@/components/CreateAccessories'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+    <CreateAccessories/>
+    </>
+  )
+}
